c1946f17f9fa7b6223cbbecaa4bdbd34926e331e
Harry <Athonal2019@gmail.com>
2024-11-16 23:02:00 +0000

    Converted invoice to class

--- invoice.py
+++ invoice.py
@@ -6,16 +6,9 @@ from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 import inspect
 from datetime import datetime
 
-styles = getSampleStyleSheet()
-
-def conv_to_paragraph(text: str, _styles = None) -> Paragraph:
-    if _styles == None:
-        _styles = styles["Normal"]
-
-    return Paragraph(text, _styles)
-
-def create_invoice(
-        filename: str = "output.pdf", 
+class Invoice:
+    def __init__(self,
+                 filename: str = "output.pdf", 
         addr_name: str = None, 
         addr_addr: str = None, 
         addr_postal: str = None, 
@@ -24,52 +17,69 @@ def create_invoice(
         invoice_ref: str = None,
         invoice_num: str = None
     ):
+        self.filename = filename
+        self.addr_name = addr_name
+        self.addr_addr = addr_addr
+        self.addr_postal = addr_postal
+        self.addr_country = addr_country
+        self.invoice_date = invoice_date
+        self.invoice_ref = invoice_ref
+        self.invoice_num = invoice_num
 
-    # Checking that all arguements have values other than None
-    frame = inspect.currentframe()
-    args = frame.f_locals
-    function_args = {key: value for key, value in args.items() if key not in ['frame']}
+        self.styles = getSampleStyleSheet()
 
-    for key, value in function_args.items():
-        if value == None:
-            raise Exception(f"Argument {key} must have a supplied value")
+    def conv_to_paragraph(self, text: str, _styles = None) -> Paragraph:
+        if _styles == None:
+            _styles = self.styles["Normal"]
 
-    document = SimpleDocTemplate(filename, pagesize=A4)
-    elements = []
+        return Paragraph(text, _styles)
 
-    invoice_date = invoice_date.strftime("%d/%m/%Y %H:%M %p")
-    BOLD_STYLE = ParagraphStyle(name="Bold", parent=styles["Normal"], fontName="Helvetica-Bold")
+    def create(self, filename: str = "output.pdf"):
+        # Checking that all arguements have values other than None
+        frame = inspect.currentframe()
+        args = frame.f_locals
+        function_args = {key: value for key, value in args.items() if key not in ['frame']}
 
-    invoice_title = Paragraph("SALES INVOICE", styles["Heading2"])
+        for key, value in function_args.items():
+            if value == None:
+                raise Exception(f"Argument {key} must have a supplied value")
 
-    invoice_data_table_data = [
-        [conv_to_paragraph("Invoice Date", BOLD_STYLE), invoice_date], 
-        [conv_to_paragraph("Invoice Reference", BOLD_STYLE), invoice_ref], 
-        [conv_to_paragraph("Invoice Number", BOLD_STYLE), invoice_num]
-    ]
-    invoice_data_table = Table(invoice_data_table_data)
-    invoice_data_table.setStyle(TableStyle([
-        ("VALIGN", (0, 0), (-1, -1), "TOP")
-    ]))
+        document = SimpleDocTemplate(filename, pagesize=A4)
+        elements = []
 
-    table_data = [[
-        [
-            invoice_title, 
-            conv_to_paragraph(addr_name), 
-            conv_to_paragraph(addr_addr), 
-            conv_to_paragraph(addr_postal), 
-            conv_to_paragraph(addr_country)
-         ], 
-         invoice_data_table
-    ]]
+        invoice_date = self.invoice_date.strftime("%d/%m/%Y %H:%M %p")
+        BOLD_STYLE = ParagraphStyle(name="Bold", parent=self.styles["Normal"], fontName="Helvetica-Bold")
 
-    table = Table(table_data, colWidths=["*", "*"])
-    table.setStyle(TableStyle([
-        ("VALIGN", (0, 0), (-1, -1), "TOP"),  # Align both cells to the top
-        ("BOTTOMPADDING", (0, 0), (-1, -1), 0),  # Reduce extra padding
-        ("TOPPADDING", (0, 0), (-1, -1), 0),
-    ]))
+        invoice_title = Paragraph("SALES INVOICE", self.styles["Heading2"])
 
-    elements.append(table)
+        invoice_data_table_data = [
+            [self.conv_to_paragraph("Invoice Date", BOLD_STYLE), invoice_date], 
+            [self.conv_to_paragraph("Invoice Reference", BOLD_STYLE), self.invoice_ref], 
+            [self.conv_to_paragraph("Invoice Number", BOLD_STYLE), self.invoice_num]
+        ]
+        invoice_data_table = Table(invoice_data_table_data)
+        invoice_data_table.setStyle(TableStyle([
+            ("VALIGN", (0, 0), (-1, -1), "TOP")
+        ]))
 
-    document.build(elements)
+        table_data = [[
+            [
+                invoice_title, 
+                self.conv_to_paragraph(self.addr_name), 
+                self.conv_to_paragraph(self.addr_addr), 
+                self.conv_to_paragraph(self.addr_postal), 
+                self.conv_to_paragraph(self.addr_country)
+            ], 
+            invoice_data_table
+        ]]
+
+        table = Table(table_data, colWidths=["*", "*"])
+        table.setStyle(TableStyle([
+            ("VALIGN", (0, 0), (-1, -1), "TOP"),  # Align both cells to the top
+            ("BOTTOMPADDING", (0, 0), (-1, -1), 0),  # Reduce extra padding
+            ("TOPPADDING", (0, 0), (-1, -1), 0),
+        ]))
+
+        elements.append(table)
+
+        document.build(elements)
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from invoice import create_invoice
+from invoice import Invoice
 
 
 if __name__ == "__main__":
-    create_invoice(
+    invoice = Invoice(
         addr_name="Google London", 
         addr_addr="6 Pancras Sq, London", 
         addr_postal="N1C 4AG", 
@@ -10,3 +10,5 @@ if __name__ == "__main__":
         invoice_ref="REF-0001",
         invoice_num=1
     )
+
+    invoice.create()
