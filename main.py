0b6a0b852bd640e63322d0514ac326f3ad5c997d
Harry <Athonal2019@gmail.com>
2024-11-24 19:19:51 +0000

    Changed path to dynamic in directory

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 from invoice import Invoice
+import os
 
 if __name__ == "__main__":
     invoice = Invoice(
-        rec_addr_name="Google London", 
-        rec_addr_addr="6 Pancras Sq",
-        rec_addr_city="London", 
-        rec_addr_postal="N1C 4AG", 
+        rec_addr_name="My Business Name", 
+        rec_addr_addr="104 Business Lane",
+        rec_addr_city="Manchester", 
+        rec_addr_postal="M5 4DG", 
         rec_addr_country="United Kingdom",
-        sndr_addr_name="My Business",
-        sndr_addr_addr="My Business Address",
-        sndr_addr_city="My Business City",
-        sndr_addr_postal="My Business Postal",
+        sndr_addr_name="Google London",
+        sndr_addr_addr="6 Pancras Sq",
+        sndr_addr_city="London",
+        sndr_addr_postal="N1C 4AG",
         sndr_addr_country="United Kingdom",
-        invoice_ref="REF-0001",
-        invoice_num=1,
+        invoice_ref="Google Ads Spend Oct-Nov",
+        invoice_num="INV-0001",
         items=[
-            ["Web hosting", 1, 30, 20],
-            ["Social media management", 1, 60, 20],
-            ["Logo graphic redesigns", 3, 15, 20]
+            ["Promoted Campaign", 1, 29.00],
+            ["Promoted Campaign", 1, 55.60],
+            ["Sponsored Post", 1, 22.10],
         ]
     )
 
     invoice.create(
         filename="output.pdf",
-        image_path=r"C:\Users\Athon\Desktop\Programming Projects\Invoice Generator\static\images\logo.png" ,
-        image_size=(100, 100)   
+        image_path=os.getcwd() + r"\static\images\logo.png",
+        image_size=(150, 51)   
     )
