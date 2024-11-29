f5491b079098014f1f3159b3c5ae6520c5ea5ee5
Harry <Athonal2019@gmail.com>
2024-11-29 19:38:57 +0000

    added function for adding items to the invoice items table

--- static/script.js
+++ static/script.js
@@ -10,4 +10,14 @@ function changeText() {
     currentIndex = (currentIndex + 1) % phrases.length;
 }
 
+function addRow() {
+    const table = document.getElementById("invoice-items").getElementsByTagName("tbody")[0];
+    const newRow = table.insertRow();
+    newRow.innerHTML = `
+      <td><input type="text" name="description[]" required></td>
+      <td><input type="number" name="quantity[]" min="1" required></td>
+      <td><input type="number" name="unit_price[]" step="0.01" required></td>
+    `;
+}
+
 setInterval(changeText, 2000);
