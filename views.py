178426e9795fb65f258653b79eda8ec7e2ba3bed
Harry <Athonal2019@gmail.com>
2024-12-07 23:21:08 +0000

    Added login, logout, register & account views

--- views.py
+++ views.py
@@ -2,16 +2,20 @@ from flask import Blueprint, render_template
 import os
 from datetime import datetime
 
-from flask import render_template, request, send_file, redirect, url_for
+from flask import render_template, request, send_file, redirect, url_for, flash
+from flask_login import login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from invoices import Invoice
+from models import User
 from app import FILE_NAME
+from database import db
 
 views = Blueprint("views", __name__)
 
 @views.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
+    return render_template("index.html", user=current_user)
 
 @views.route("/customise", methods=["GET", "POST"])
 def customise():
@@ -72,3 +76,52 @@ def file():
 @views.route("/after_download")
 def after_download():
     return redirect(url_for("views.index"))
+
+@views.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        if password1 == password2:
+            new_user = User(email=email, name=name, password=generate_password_hash(password1, method="pbkdf2:sha256"))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user)
+
+        return redirect(url_for("views.index"))
+    
+    return render_template("register.html", user=current_user)
+
+@views.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            print(user.password, password)
+            if check_password_hash(user.password, password):
+                login_user(user, remember=True)
+                
+                return redirect(url_for("views.account"))
+            else:
+                flash("Incorrect email or password", category="error")
+        else:
+            flash("No account with this email found. Please create an account", category="error")
+        
+    return render_template("login.html", user=current_user)
+
+@login_required
+@views.route("/account")
+def account():
+    return render_template("account.html", user=current_user)
+
+@login_required
+@views.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("views.login"))
